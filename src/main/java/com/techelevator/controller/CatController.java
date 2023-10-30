b622c7e8890ef605febd299486ed28c9e4aba750
Nigus Gebremariam <kingsgm19@gmail.com>
2023-10-29 20:54:15 -0400

    Complete the services and couple methods

--- src/main/java/com/techelevator/controller/CatController.java
+++ src/main/java/com/techelevator/controller/CatController.java
@@ -1,9 +1,20 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
+import com.techelevator.model.CatFact;
+import com.techelevator.model.CatPic;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping ("/api/cards/")
 public class CatController {
 
     private CatCardDao catCardDao;
@@ -15,6 +26,27 @@ public class CatController {
         this.catFactService = catFactService;
         this.catPicService = catPicService;
     }
+
+    @GetMapping
+    public List<CatCard> getCatCards() {
+        return catCardDao.getCatCards();
+    }
+
+    @GetMapping ("/{id}")
+    public CatCard getCatCardById (@PathVariable ("id") int id) {
+        return catCardDao.getCatCardById(id);
+    }
+
+    @GetMapping ("/random")
+    public CatCard getCatCardRandomly () {
+        CatFact catFact = catFactService.getFact();
+        CatPic catPic = catPicService.getPic();
+        CatCard catCard = new CatCard();
+        catCard.setCatFact(catFact.getText());
+        catCard.setImgUrl(catPic.getFile());
+        return catCard;
+    }
+
 
 
 }
--- src/main/java/com/techelevator/services/RestCatFactService.java
+++ src/main/java/com/techelevator/services/RestCatFactService.java
@@ -3,14 +3,17 @@ package com.techelevator.services;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatFact;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatFactService implements CatFactService {
 
+	private final String catFactUrl = "https://cat-data.netlify.app/api/facts/random";
+	private final RestTemplate restTemplate = new RestTemplate();
+
 	@Override
 	public CatFact getFact() {
-		// TODO Auto-generated method stub
-		return null;
+		return restTemplate.getForObject(catFactUrl, CatFact.class);
 	}
 
 }
